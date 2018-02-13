49df36946629700b78bed7ddb6c5da03d07e941c
Tyler lemke <tyleris@gmail.com>
2018-02-12 20:56:00 -0600

    more array examples

--- DataTypes/arrays.js
+++ DataTypes/arrays.js
@@ -26,9 +26,15 @@ exampleArray.filter(function(number) {return number === 1;}); //return 1 since t
 //join() - same as toString , but concats with a chosen modifier
 exampleArray.join(' '); // return '1 2 3'
 
-//splice() - delete one or more array items and replaces them with the provided value
-example.Array.splice(1, 1, 1); // returns [1, 1, 3] //TODO double check this output
+//splice() - delete one or more array items and replaces them with the provided value - mutates the array
+//used when you want to delete an item from an array - instead of delete array[0] - because doing that leave array[0] undefined
+exampleArray.splice(0, 2, 55); // returns [1, 2] and makes exampleArray [55, 3]
 
-//forEach() -
+//forEach() - run a function on every element in an array
+//Signature from MDN
+//arr.forEach(function callback(currentValue[, index[, array]]) {
+//    //your iterator
+//}[, thisArg])
+exampleArray.forEach(callbackFunction);
 
 //concat() -
