6db9cf33cd5349afec9dfdeebd22c665102b6ac8
Tyler lemke <tyleris@gmail.com>
2018-02-16 23:04:44 -0600

    adding operators

---
+++ operators.js
@@ -0,0 +1,40 @@
+/* Hello, Operator? Can you please help me assign this?
+Operators are commonly used but rarely respected. Lets give these
+little heroes the respect that they deserve.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
+*/
+
+//TODO add all of the gotchas listed on MDN and other locations
+
+
+//ARITHMATIC OPERATORS
+
+/*
+ *  + (Addition)
+ *  Used for addition && string concatenation
+ */
+console.log(1 + 2); // returns 3
+console.log(1 + '2'); // returns "12" as a string
+console.log('hello' + 'world'); // returns "helloworld"
+
+/*
+ *  - (Subtraction)
+ *  Use for subtracting stuff
+ */
+console.log(1 - 2); // returns -1
+console.log('number' - 1) // returns NaN
+
+/*
+ *  / (Division)
+ *  Dividing the first number by the second
+ */
+console.log(2 / 4); // returns .5
+
+/*
+ *  * (Multiplication)
+ *  Multiplying a number
+ */
+console.log(2 * 4); // returns 8
+
+
