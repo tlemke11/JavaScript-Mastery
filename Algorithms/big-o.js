7935bc1247581219a3bd9c9adf0df3b57fd7946d
Tyler Lemke <tyleris@gmail.com>
2018-02-19 20:33:06 -0600

    adding big O examples

---
+++ Algorithms/big-o.js
@@ -0,0 +1,27 @@
+/*
+*  I thought it would be cool here to demonstrate big (O) with JavaScript.
+*
+*  A Lot of Concepts on Big O are covered in Cracking the Coding Interview
+*
+*  Some concepts here were gleaned from
+*
+*  https://medium.com/cesars-tech-insights/big-o-notation-javascript-25c79f50b19b
+ */
+
+
+// O (1)
+// HashMaps are examples of O (1) Time Complexity
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+// So is returning an item from a known array index
+
+var hashmap = new Map();
+hashmap.set("key", "value");
+console.log(hashmap.get("key")); //returns value at a constant rate of 1
+
+// O (N)
+
+
+// O (N**2)
+
+
+
