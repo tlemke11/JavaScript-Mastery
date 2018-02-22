e1b259ce9c03f01185329d385d01abff4f4f3333
Tyler Lemke <tyleris@gmail.com>
2018-02-21 22:49:14 -0600

    learning about Math Static Object

---
+++ GlobalObjects/a-overview-global-objects.js
@@ -0,0 +1,11 @@
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+*
+*  There are roughtly 1 billion built in objects within JavaScript (strictly Hyberbole)
+*  I want to go through the most important, and slowly reach out into the more obscure ones
+*
+*  Todo - This is going to overflow heavily into DataTypes so we may need to restructure
+*
+*
+*  1. Math
+*
+* */
---
+++ GlobalObjects/math.js
@@ -0,0 +1,15 @@
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+   Math is a Global Static Object with static properties and methods.
+
+   Here is the list of the most useful Math Methods (this has no imperical backing)
+ */
+
+//Math.min(<firstNumber>, <secondNumber)
+//return the smallest value of two numbers
+console.log(Math.min(1,2)); //return 1;
+
+//Math.max(<firstNumber>, <secondNumber)
+//return the greatest value of two numbers
+console.log(Math.max(1,2)); //return 2;
+
