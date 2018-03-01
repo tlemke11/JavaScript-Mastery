416cabeb3a41ebb0a57b381dc8f196f9fdc5653f
Tyler Lemke <tyleris@gmail.com>
2018-02-28 23:54:00 -0600

    adding promises/callbacks

---
+++ Promises and Callbacks/Promises.js
@@ -0,0 +1,8 @@
+//What are promises?
+
+//Promises are replacements for Callbacks that allow for easier error handling and avoiding the pyramid of doom.
+
+// Promise.all();
+
+//Promise.resolve();
+//You need one of these in order to do promise chaining with .then()
