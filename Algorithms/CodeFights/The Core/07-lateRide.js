9e9cbe519b4b96090f59bb3986bd1776e0b5f039
Tyler Lemke <tyleris@gmail.com>
2018-04-16 20:44:37 -0500

    lateride try 2

--- Algorithms/CodeFights/The Core/07-lateRide.js
+++ Algorithms/CodeFights/The Core/07-lateRide.js
@@ -1,6 +1,8 @@
-//TODO finish - started !5 min so far
-function lateRide(n) {
+//TODO finish - started 20 min so far
+ffunction lateRide(n) {
     minutes = n%60;
     hours = Math.floor(n/60);
-
+    minutesSum = (minutes % 100 - minutes % 10) + minutes % 10;
+    hoursSum = (hours % 100 - hours % 10) + hours % 10;
+    return minutesSum + hoursSum;
 }
