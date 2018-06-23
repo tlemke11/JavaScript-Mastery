06e11dde52c24002b64aa3e1c84993bd416be5c2
tslemke11 <tyleris@gmail.com>
2018-06-23 14:47:13 -0500

    adding almostIncreasing before redo

--- Algorithms/CodeFights/Arcade - Intro/07-almostIncreasingSequence.js
+++ Algorithms/CodeFights/Arcade - Intro/07-almostIncreasingSequence.js
@@ -6,42 +6,45 @@
 //i.e. [1, 2, 4, 3, 4, 5] is true because if you remove just the 4 you have
 //an increasing sequence
 
-
-//1st attempt - 44 min
 function almostIncreasingSequence(sequence) {
 
-    let i = 0;
-    let j = 1;
-    let currentSequenceNumber = null;
-    let removeCounter = 0;
+let i = 0;
+let j = 1;
+let currentSequenceNumber = null;
+let removeCounter = 0;
 
-    if (sequence[i] > sequence[j]){
-        i++;
-        j++;
-        removeCounter++;
-    }
+if (sequence[i] >= sequence[j] || (sequence[i] + 1 != sequence[j])){
+    i++;
+    j++;
+    removeCounter++;
+}
 
+console.log(removeCounter);
+while((j <= sequence.length) && (removeCounter <= 1) && sequence.length > 3){
 
-    while((j <= sequence.length) && (removeCounter <= 1)){
-
-        if(currentSequenceNumber === sequence[i] || currentSequenceNumber === sequence[j]){
-            return false;
-        }
-
-        if(sequence[i] > sequence[j])  {
-            removeCounter++;
-            currentSequenceNumber = sequence[j];
-            j++;
-        } else {
-            currentSequenceNumber = sequence[j];
-            i += 2;
-            j += 2;
-        }
-    }
-
-    if(removeCounter > 1){
+    if(currentSequenceNumber === sequence[i] || currentSequenceNumber === sequence[j]){
         return false;
     }
-
-    return true;
+    console.log("this is i: " + i + " " + "and this is j: " + j)
+    if(sequence[i] >= sequence[j] || (sequence[i] + 1 != sequence[j]))  {
+        removeCounter++;
+        currentSequenceNumber = sequence[j];
+        j++;
+    } else {
+    currentSequenceNumber = sequence[j];
+    i += 2;
+    j += 2;
+    }
 }
+
+if(removeCounter > 1){
+    return false;
+}
+
+return true;
+}
+
+
+//Aglorithm
+//
+//
