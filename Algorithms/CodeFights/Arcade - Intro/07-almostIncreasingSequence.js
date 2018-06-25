bdd80fba4dc3719a0b7dbff3a5a045b19f8de04c
tslemke11 <tyleris@gmail.com>
2018-06-25 06:04:19 -0500

    improved test functions and passed first test

--- Algorithms/CodeFights/Arcade - Intro/07-almostIncreasingSequence.js
+++ Algorithms/CodeFights/Arcade - Intro/07-almostIncreasingSequence.js
@@ -1,26 +1,36 @@
 //Help with Idea courtesy of https://codefights.com/profile/exodiquas
 function almostIncreasingSequence(sequence, isTest) {
-        firstValue = sequence[0];
         removals = 0;
-        index = 0;
-        while(index < sequence.Length && removals <= 1){
-
+        firstIndex = 0;
+        secondIndex = 1;
+        while(firstIndex < sequence.Length && removals <= 1){
+                if(sequence[firstIndex] < sequence[secondIndex]){
+                    removals++;
+                    sequence.splice(secondIndex,1);
+                } else {
+                    firstIndex++;
+                    secondIndex++;
+                }
         }
     if(!isTest){
         runTests();
     }
+    return (removals < 2);
 }
 
-
 //Unit Tests
-function runTests(){
-    console.log("TestRunner");
-    testArray = [
+var testArray = [
         {
         'testName': 'simpleSequenceOneTwoThree',
         'testValues': [1,2,3]
         }
-                ];
+];
+
+//Test Runner
+function runTests(){
+    console.log("TestRunner");
+
+
     failedTests = 0;
 
     for (var i = 0; i < testArray.length; i++){
@@ -34,10 +44,9 @@ function runTests(){
     } else {
         console.log("All Tests Passed")
     }
-
-
 }
 
+//Test Helper
 function testIncreasingSequence(testSequence, testName){
     if (almostIncreasingSequence(testSequence, true)){
         console.log(testName + "Passed");
