f01fac1eb50679b64ef6660997ce3f5d86680951
tslemke11 <tyleris@gmail.com>
2018-06-26 06:43:50 -0500

    refactored test runner

--- Algorithms/CodeFights/Arcade - Intro/07-almostIncreasingSequence.js
+++ Algorithms/CodeFights/Arcade - Intro/07-almostIncreasingSequence.js
@@ -1,58 +1,55 @@
 //Help with Idea courtesy of https://codefights.com/profile/exodiquas
 function almostIncreasingSequence(sequence, isTest) {
-        removals = 0;
-        firstIndex = 0;
-        secondIndex = 1;
-        while(firstIndex < sequence.Length && removals <= 1){
-                if(sequence[firstIndex] < sequence[secondIndex]){
-                    removals++;
-                    sequence.splice(secondIndex,1);
-                } else {
-                    firstIndex++;
-                    secondIndex++;
-                }
-        }
-    if(!isTest){
-        runTests();
+    removals = 0;
+    firstIndex = 0;
+    secondIndex = 1;
+    while(firstIndex < sequence.Length && removals <= 1){
+            if(sequence[firstIndex] < sequence[secondIndex]){
+                removals++;
+                sequence.splice(secondIndex,1);
+            } else {
+                firstIndex++;
+                secondIndex++;
+            }
     }
-    return (removals < 2);
+if(!isTest){
+    runTests();
+}
+return false;
+//return (removals < 2);
 }
 
 //Unit Tests
 var testArray = [
-        {
-        'testName': 'simpleSequenceOneTwoThree',
-        'testValues': [1,2,3]
-        }
+    {
+    'testName': 'Sequence 1,2,3 Asserts True',
+    'testValues': [1,2,3],
+    'assert': true
+    },
+{
+    'testName': 'Sequence 1,3,2,1 Asserts True',
+    'testValues': [1,3,2,1],
+    'assert': true
+}
 ];
 
 //Test Runner
 function runTests(){
-    console.log("TestRunner");
+console.log("TestRunner");
+failedTests = 0;
 
-
-    failedTests = 0;
-
-    for (var i = 0; i < testArray.length; i++){
-         if(!testIncreasingSequence(testArray[0].testValues,testArray[0].testName)){
-             failedTests++;
-         };
-    }
-
-    if(failedTests != 0){
-        console.log(failedTests + " Test(s) Failed")
-    } else {
-        console.log("All Tests Passed")
-    }
+for (var i = 0; i < testArray.length; i++){
+     if(almostIncreasingSequence(testArray[i].testValues,testArray[i].testName) != testArray[i].assert){
+         failedTests++;
+         console.log(testArray[i].testName + " FAILED!!");
+     } else {
+         console.log(testArray[i].testName + " Passed");
+     }
 }
 
-//Test Helper
-function testIncreasingSequence(testSequence, testName){
-    if (almostIncreasingSequence(testSequence, true)){
-        console.log(testName + "Passed");
-        return true;
-    } else {
-        console.log(testName + "Failed")
-        return false;
-    }
+if(failedTests != 0){
+    console.log(failedTests + " Test(s) Failed")
+} else {
+    console.log("All Tests Passed")
+}
 }
